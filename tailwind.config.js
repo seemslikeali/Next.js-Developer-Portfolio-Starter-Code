86e329ecc50751fccb56c35c095963810dd5567c
Ali <seems.like.ali@gmail.com>
2023-05-10 16:41:04 -0600

    almost done navbar
    45 mins in

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,19 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         mont: ['var(--font-mont)', ...fontFamily.sans],
         mono: ['var(--font-roboto-mono)'],
       },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      }
     },
   },
   plugins: [],
