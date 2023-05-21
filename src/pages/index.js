6e22cd480a83153a5a1e77f49798011b0da81bfb
Ali <seems.like.ali@gmail.com>
2023-05-21 07:00:50 -0600

    ADDED THE START OF LAYOUT

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+import Layout from '@/components/Layout'
 import Head from 'next/head'
-
+import Image from 'next/image'
+import profilePic from "../../public/images/profile/developer-pic-1.png"
 
 export default function Home() {
   return (
@@ -8,8 +10,14 @@ export default function Home() {
         <title>Create Next App</title>
         <meta name="description" content="" />
       </Head>
-      <main className="">
-        Home
+      <main className=" flex items-center text-dark w-full min-h-screen">
+        <Layout className='pt-0'>
+          <div className="flex items-center justify-between w-full">
+            <div className='w-1/2'>
+              <Image src={profilePic} alt="Ali" className='w-full h-auto' />
+            </div>
+          </div>
+        </Layout>
       </main>
     </>
   )
