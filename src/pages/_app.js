42dfbef796245997fcb8669cf94459c5481ccfbe
Ali Shakeel <seems.like.ali@gmail.com>
2023-05-21 14:05:51 -0600

    finished home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Montserrat } from "next/font/google"
 import Head from "next/head"
 import NavBar from '@/components/NavBar'
+import Footer from '@/components/Footer'
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }) {
 
         <NavBar />
         <Component {...pageProps} />
+        <Footer />
       </main>
 
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import profilePic from "../../public/images/profile/developer-pic-1.png"
+import AnimatedText from '@/components/AnimatedText'
+import { LinkArrow } from '@/components/Icons'
+import HireMe from '@/components/HireMe'
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg"
 
 export default function Home() {
   return (
@@ -16,8 +21,31 @@ export default function Home() {
             <div className='w-1/2'>
               <Image src={profilePic} alt="Ali" className='w-full h-auto' />
             </div>
+
+            <div className='w-1/2 flex flex-col items-center self-center'>
+
+              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left' />
+              <p className='my-4 text-base font-medium'>
+                As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+                Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+              </p>
+              <div className='flex items-center self-start mt-2'>
+                <Link download={true} className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark' href="/dummy.pdf" target='_blank'
+                >
+                  Resume <LinkArrow className={"w-6 ml-1.5"}></LinkArrow>
+                </Link>
+                <Link href={"mailto:seems.like.ali@gmail.com"} target='_blank'
+                  className='ml-4 text-lg font-medium capitalize text-dark underline'
+                >Contact</Link>
+              </div>
+            </div>
           </div>
         </Layout>
+
+        <HireMe />
+        <div className='absolute right-8 bottom-8 inline-block w-24'>
+          <Image src={lightBulb} alt='Ali Shakeel' className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
