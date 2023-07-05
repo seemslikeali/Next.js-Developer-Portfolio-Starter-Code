78c74c7aee12ceb54b709962d9bcbf27e3118a2c
Ali Shakeel <seems.like.ali@gmail.com>
2023-07-05 07:06:12 -0600

    Update contactus.js

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -149,7 +149,7 @@ const articles = () => {
 
                         <section class="mb-32">
                             <div
-                                class="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat bg-[url('https://mdbcdn.b-cdn.net/img/new/textures/full/171.jpg')]">
+                                class="relative h-[300px] overflow-hidden bg-cover bg-[50%] bg-no-repeat bg-[url('https://images.unsplash.com/photo-1593291600904-660bbd59a758?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')]">
                             </div>
                             <div class="container px-6 mdl:px-12">
                                 <div
@@ -161,7 +161,7 @@ const articles = () => {
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
                                             </svg>
-                                            <h6 class="font-medium">Unites States</h6>
+                                            <h6 class="font-medium">Canada</h6>
                                         </div>
                                         <div class="mx-auto mb-12 text-center lgl:mb-0">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
@@ -170,7 +170,7 @@ const articles = () => {
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                                             </svg>
-                                            <h6 class="font-medium">New York, 94126</h6>
+                                            <h6 class="font-medium">Saskatchewan, S7W</h6>
                                         </div>
                                         <div class="mx-auto mb-6 text-center mdl:mb-0">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
@@ -178,7 +178,7 @@ const articles = () => {
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                                             </svg>
-                                            <h6 class="font-medium">+ 01 234 567 89</h6>
+                                            <h6 class="font-medium">+ 1 306 850 1213</h6>
                                         </div>
                                         <div class="mx-auto text-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
@@ -186,7 +186,7 @@ const articles = () => {
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
                                             </svg>
-                                            <h6 class="font-medium">Tax ID: 273 384</h6>
+                                            <h6 class="font-medium">Tax ID: unavailable</h6>
                                         </div>
                                     </div>
                                     <div class="mx-auto max-w-[700px]">
@@ -232,7 +232,10 @@ const articles = () => {
 
 
 
+
                 </Layout>
+
+
             </main>
         </>
     )
